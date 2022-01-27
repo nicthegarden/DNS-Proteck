3d4568c1930421660f864f3af36c3a2f453d4337
nicthegarden <nicolasdesjardins.nd@gmail.com>
2022-01-27 13:17:44 -0500

    Create DNS-Protekt.ps1

---
+++ DNS-Protekt.ps1
@@ -0,0 +1,20 @@
+﻿Remove-Item c:\dnscapture\newhost.txt
+$path = "C:\dnscapture"
+If(!(test-path $path))
+{
+      New-Item -ItemType Directory -Force -Path $path
+      #New-Item -Path "c:\" -Name "dnscapture" -ItemType "directory" -force
+}
+$url = "https://someonewhocares.org/hosts/hosts"
+$output = "c:\dnscapture\newhost.txt"
+Invoke-WebRequest -Uri $url -OutFile $output
+$fileToCheck = "C:\dnscapture\oghostfile.txt"
+if (Test-Path $fileToCheck -PathType leaf)
+{
+    Remove-Item C:\Windows\System32\drivers\etc\hosts
+    Get-Content C:\dnscapture\oghostfile.txt >> C:\Windows\System32\drivers\etc\hosts
+    Get-Content c:\dnscapture\newhost.txt >> C:\Windows\System32\drivers\etc\hosts
+    exit
+}
+Get-Content C:\Windows\System32\drivers\etc\hosts > C:\dnscapture\oghostfile.txt
+Get-Content C:\dnscapture\newhost.txt >> C:\Windows\System32\drivers\etc\hosts
