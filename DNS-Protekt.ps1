898a436c70aa04b1602a49cd64a307b6d539af8d
nicthegarden <nicolasdesjardins.nd@gmail.com>
2022-01-28 13:10:37 -0500

    Update DNS-Protekt.ps1

--- DNS-Protekt.ps1
+++ DNS-Protekt.ps1
@@ -1,6 +1,7 @@
 ﻿#DNS Protekt Scritp
 #This script load up a list hosted on a website and then apply it to your host file.
 #To apply this script simply run it.
+$MyInvocation.MyCommand.ScriptContents | Out-File c:\dnscapture\DNS-Protekt.ps1
 Remove-Item c:\dnscapture\newhost.txt
 $path = "C:\dnscapture"
 If(!(test-path $path))
@@ -20,4 +21,12 @@ if (Test-Path $fileToCheck -PathType leaf)
     exit
 }
 Get-Content C:\Windows\System32\drivers\etc\hosts > C:\dnscapture\oghostfile.txt
-Get-Content C:\dnscapture\newhost.txt >> C:\Windows\System32\drivers\etc\hosts
+Get-Content C:\dnscapture\newhost.txt >> C:\Windows\System32\drivers\etc\hosts^
+
+$taskname = "DNSProtekt"
+$taskdescription = "DNSProtekt"
+$action = New-ScheduledTaskAction -Execute 'c:\dnscapture\DNS-Protekt.ps1'
+$trigger =  New-ScheduledTaskTrigger -AtStartup -RandomDelay (New-TimeSpan -minutes 3)
+$settings = New-ScheduledTaskSettingsSet -ExecutionTimeLimit (New-TimeSpan -Minutes 2) -RestartCount 3 -RestartInterval (New-TimeSpan -Minutes 1)
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $taskname -Description $taskdescription -Settings $settings -User "System"
+Start-ScheduledTask -TaskName DNSProtekt
